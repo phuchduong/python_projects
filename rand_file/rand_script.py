e1f16fadad2ff68f9c3095d88afee68af776752a
Phuc Duong <phuchduong@hotmail.com>
2016-06-25 10:55:35 -0700

    added a reroll function

--- rand_file/rand_script.py
+++ rand_file/rand_script.py
@@ -1,11 +1,22 @@
-base_dir = 'E:\\xDocuments\\xOther'
+base_dir = 'F:\\xDocuments\\xOther'
 
 import os
+from rand_file import rand_file
 
 # Sets to the current folder path
 current_path = os.path.dirname(os.path.realpath(__file__))
 os.chdir(current_path)
 
-from rand_file import rand_file
 
-file_tree = rand_file(base_dir)
+def get_rand_file():
+    file_list = rand_file(base_dir)
+    input("Press Enter for another file...")
+    get_another_file(file_list)
+
+
+def get_another_file(file_list):
+    file_list.re_roll()
+    input("Press Enter for another file...")
+    get_another_file(file_list)
+
+get_rand_file()
