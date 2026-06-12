cbde150fe4fd0b9c104e217d065dc3717e8ee8ed
Phuc H Duong <phuchduong@hotmail.com>
2026-06-12 16:06:24 -0700

    qc

---
+++ rand_file/rand_script - qc.py
@@ -0,0 +1,24 @@
+import os
+from rand_file import RandFile
+
+base_dir = 'U:\\qBT\\completed1\\'
+program = "C:\\Program Files (x86)\\K-Lite Codec Pack\\MPC-HC64\\mpc-hc64.exe"
+
+# Sets to the current folder path
+current_path = os.path.dirname(os.path.realpath(__file__))
+os.chdir(current_path)
+
+
+def get_RandFile():
+    file_list = RandFile(base_dir, program)
+    input("Press Enter for another file...")
+    get_another_file(file_list)
+
+
+def get_another_file(file_list):
+    file_list.re_roll()
+    input("Press Enter for another file...")
+    get_another_file(file_list)
+
+
+get_RandFile()
